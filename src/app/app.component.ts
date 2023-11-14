e6565d6c5dfddcfd1059f345938911c095c177e3
Sanjar Suyunov <sanjarsuyunov1304@gmail.com>
2023-11-14 14:55:11 +0500

    Added environment file and configured angular.json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+
+import { filter, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'starwars-task';
+export class AppComponent implements OnInit {
+  
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private title: Title,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    this.setPageTitle();
+  }
+
+  private setPageTitle(): void {
+    const defaultPageTitle = 'Star Wars';
+
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd),
+      map(() => {
+        let child = this.activatedRoute.firstChild;
+
+        if (!child) {
+          return this.activatedRoute.snapshot.data['pageTitle'] || defaultPageTitle;
+        }
+
+        while (child.firstChild) {
+          child = child.firstChild;
+        }
+
+        if (child.snapshot.data['pageTitle']) {
+          return child.snapshot.data['pageTitle'] || defaultPageTitle;
+        }
+      })
+    ).subscribe((title: string) => this.title.setTitle(title));
+  }
+
 }
