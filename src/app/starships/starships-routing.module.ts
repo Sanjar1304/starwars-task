8769d661fad98a59d84075018f4057f72b2f8f52
Sanjar Suyunov <sanjarsuyunov1304@gmail.com>
2023-11-14 14:35:01 +0500

    Configured starships comp. module and services

--- src/app/starships/starships-routing.module.ts
+++ src/app/starships/starships-routing.module.ts
@@ -1,12 +1,26 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+import { StarshipComponent } from './starship/starship.component';
+import { StarshipsComponent } from './starships.component';
 
 
+const routes: Routes = [
+  {
+    path: '',
+    component: StarshipsComponent
+  },
+  {
+    path: ':id',
+    component: StarshipComponent,
+    data: {
+      pageTitle: 'View Starship'
+    }
+  }
+];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
 })
 export class StarshipsRoutingModule { }
