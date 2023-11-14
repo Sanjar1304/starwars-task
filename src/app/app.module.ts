5eba1724314862fb483628283de003990828ed00
Sanjar Suyunov <sanjarsuyunov1304@gmail.com>
2023-11-14 11:36:35 +0500

    Configured app.module.ts file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,46 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
-import { CharactersComponent } from './characters/characters.component';
-import { CharacterComponent } from './characters/character/character.component';
-import { FilmsComponent } from './films/films.component';
-import { FilmComponent } from './films/film/film.component';
 import { HomeComponent } from './home/home.component';
-import { ListComponent } from './list/list.component';
-import { LoadingComponent } from './loading/loading.component';
-import { PlanetsComponent } from './planets/planets.component';
-import { PlanetComponent } from './planets/planet/planet.component';
-import { SpeciesComponent } from './species/species.component';
-import { SpecieComponent } from './species/specie/specie.component';
-import { StarshipsComponent } from './starships/starships.component';
-import { StarshipComponent } from './starships/starship/starship.component';
-import { VehiclesComponent } from './vehicles/vehicles.component';
-import { VehicleComponent } from './vehicles/vehicle/vehicle.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CharactersComponent,
-    CharacterComponent,
-    FilmsComponent,
-    FilmComponent,
+    AppComponent, 
     HomeComponent,
-    ListComponent,
-    LoadingComponent,
-    PlanetsComponent,
-    PlanetComponent,
-    SpeciesComponent,
-    SpecieComponent,
-    StarshipsComponent,
-    StarshipComponent,
-    VehiclesComponent,
-    VehicleComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
